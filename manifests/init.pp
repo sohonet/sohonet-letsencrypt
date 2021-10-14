f922105c52ab31d23db1271a8e3d4c0011f23541
Stephen Boyd <stephen.boyd@sohonet.com>
2021-10-14 12:52:13 -0700

    Add some basic init

---
+++ manifests/init.pp
@@ -0,0 +1,35 @@
+# @summary Manage a letsencrypt setup
+class letsencrypt (
+  Stdlib::Fqdn $site_fqdn,
+  Array $commandline_args = [],
+  Boolean $disable_all_flags = false,
+  Stdlib::Absolutepath $virtualenv_path = '/var/letsencrypt',
+  String $certbot_version = 'v1.19.0',
+) {
+
+  Exec {
+    cwd => $virtualenv_path,
+  }
+
+  file { 'Virtual Environment Directory':
+    ensure => directory,
+    path   => $virtualenv_path,
+  }
+  ~> exec { 'Create Virtual Env':
+    command      => '/usr/bin/env pipenv --python python3',
+    refresh_only => true,
+  }
+  -> exec { 'Bootstrap Pip':
+    command => '/usr/bin/env pipenv run pip install --upgrade pip',
+  }
+  -> exec { 'Install Dependencies':
+    command => "/usr/bin/env pipenv run pip install certbot==${certbot_version}",
+  }
+
+  file { 'Certbot Config File':
+    ensure  => file,
+    content => template('config.ini.erb'),
+    path    => "${virtualenv_path}/config.ini",
+  }
+
+}
