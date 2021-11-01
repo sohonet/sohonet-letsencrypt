9d7f4f2724cd23669d21b22fe2c7e65abcb00753
Stephen Boyd <stephen.boyd@sohonet.com>
2021-11-01 10:19:26 -0700

    Extreme measures

--- files/firstrun.sh
+++ files/firstrun.sh
@@ -16,12 +16,7 @@ post_hook="$5"
 authenticator="$6"
 webroot_paths="$7"
 alt_names="$8"
-
 actual_webroot_paths=$(printf '%s' "${webroot_paths##-w}")
-
-if [ -z "$actual_webroot_paths" ]; then
-    webroot_paths=
-fi
 
 if [ -n "$alt_names" ]; then
     siteFqdn="$siteFqdn,$alt_names"
@@ -29,14 +24,29 @@ fi
 
 cd "$virtualEnvDir" || exit 1
 
-/usr/bin/env pipenv run certbot \
-    -c "$virtualEnvDir"/config.ini \
-    --quiet \
-    certonly \
-    --cert-name "$siteFqdn" \
-    -d "$siteFqdn" \
-    --agree-tos \
-    --email "$email" \
-    --pre-hook "\"$pre_hook\"" \
-    --post-hook "\"$post_hook\"" \
-    --authenticator "$authenticator" "$webroot_paths"
+if [ -z "$actual_webroot_paths" ]; then
+    /usr/bin/env pipenv run certbot \
+        -c "$virtualEnvDir"/config.ini \
+        --quiet \
+        certonly \
+        --cert-name "$siteFqdn" \
+        -d "$siteFqdn" \
+        --agree-tos \
+        --email "$email" \
+        --pre-hook "\"$pre_hook\"" \
+        --post-hook "\"$post_hook\"" \
+        --authenticator "$authenticator"
+else
+    /usr/bin/env pipenv run certbot \
+        -c "$virtualEnvDir"/config.ini \
+        --quiet \
+        certonly \
+        --cert-name "$siteFqdn" \
+        -d "$siteFqdn" \
+        --agree-tos \
+        --email "$email" \
+        --pre-hook "\"$pre_hook\"" \
+        --post-hook "\"$post_hook\"" \
+        --authenticator "$authenticator" \
+        "$webroot_paths"
+fi
