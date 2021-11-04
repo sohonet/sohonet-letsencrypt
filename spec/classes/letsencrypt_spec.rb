9529fda750b36f195e24911af18ae4c2e58dd616
Stephen Boyd <stephen.boyd@sohonet.com>
2021-11-04 15:46:15 -0700

    Add more tests for sanity

--- spec/classes/letsencrypt_spec.rb
+++ spec/classes/letsencrypt_spec.rb
@@ -57,6 +57,10 @@ describe 'letsencrypt' do
         it 'should complete basic compilation' do
             is_expected.to compile.with_all_deps
         end
+
+        it 'should not contain any plugin execs' do
+            is_expected.not_to contain_exec('Install dns-route53')
+        end
     end
 
     context 'with multiple certificates and all optional params' do
@@ -87,4 +91,79 @@ describe 'letsencrypt' do
             is_expected.to compile.with_all_deps
         end
     end
+
+    context 'with a single plugin' do
+        let(:pre_condition) {
+            'package { "pipenv": }'
+        }
+
+        let(:params) {
+            {
+                'plugins' => [
+                    'dns-route53',
+                ],
+                'configs' => [
+                    {
+                        'site_fqdn' => 'example.foo.com',
+                        'email' => 'test@example.com',
+                        'pre_hook' => '/usr/bin/foo',
+                        'post_hook' => '/usr/bin/bar',
+                    },
+                    {
+                        'site_fqdn' => 'example.bar.com',
+                        'email' => 'test@bar.com',
+                        'pre_hook' => '/usr/bin/buzz',
+                        'post_hook' => '/usr/bin/baz',
+                    },
+                ]
+            }
+        }
+
+        it 'should complete basic compilation' do
+            is_expected.to compile.with_all_deps
+        end
+
+        it 'should install the plugin' do
+            is_expected.to contain_exec('Install dns-route53')
+        end
+    end
+
+    context 'with mutliple plugins' do
+        let(:pre_condition) {
+            'package { "pipenv": }'
+        }
+
+        let(:params) {
+            {
+                'plugins' => [
+                    'dns-route53',
+                    'dns-route52',
+                ],
+                'configs' => [
+                    {
+                        'site_fqdn' => 'example.foo.com',
+                        'email' => 'test@example.com',
+                        'pre_hook' => '/usr/bin/foo',
+                        'post_hook' => '/usr/bin/bar',
+                    },
+                    {
+                        'site_fqdn' => 'example.bar.com',
+                        'email' => 'test@bar.com',
+                        'pre_hook' => '/usr/bin/buzz',
+                        'post_hook' => '/usr/bin/baz',
+                    },
+                ]
+            }
+        }
+
+        it 'should complete basic compilation' do
+            is_expected.to compile.with_all_deps
+        end
+
+        it 'should install the plugins' do
+            is_expected.to contain_exec('Install dns-route53')
+            is_expected.to contain_exec('Install dns-route52')
+        end
+    end
+
 end
